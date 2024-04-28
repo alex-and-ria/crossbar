1c5e33b4116c0f4b84b35017fe7d6c14414147ad
alex-and-ria <alex.and.riaqqq@gmail.com>
2024-04-27 20:49:21 -0400

    Revert "added cell_crossbar.nb; added gluing .sh;"
    This reverts commit b95ea94066b3748388a192fed2b64ffb0fa9d4e3.

--- hspice_parse.cpp
+++ hspice_parse.cpp
@@ -34,25 +34,12 @@
 #include<fstream>//ifstream;
 #include<string.h>//strtok;
 
-//#define ifn "./hspice/3x3_crossbar_gen.ic0"
-//#define M 3
-//#define N 3
+#define ifn "./hspice/3x3_crossbar_gen.ic0"
+#define M 3
+#define N 3
 #define ibuff_size 256
-#define debug 0
-	
 
-int main(int argc, char** argv){
-	char* ifn;
-	unsigned int M,N;
-	if(argc!=4){
-		std::cout<<"\nusage: parse_obj input_file_path M N";	
-	}
-	else{
-		ifn=argv[1];
-		M=atoi(argv[2]);
-		N=atoi(argv[3]);
-	
-	}
+int main(){
 	std::ifstream ifh(ifn,std::ios::in|std::ios::binary);
 	unsigned char ibuff[ibuff_size]; char get_nl; unsigned char* uc_arr_tkn;
 	bool strt_fl=0; unsigned int num_nodes=2*M*N+M;//2*m*n nodes for crossbar itself, m nodes for input sources + ground node (which is 0 V);
@@ -63,14 +50,14 @@ int main(int argc, char** argv){
 			//std::cout<<'\n'<<"line:"<<ibuff;
 			ifh.get(get_nl);//read '\n' character from stream to allow getting new line on next iteration;
 			if(strt_fl==0 && strstr((char*)ibuff,".nodeset")!=NULL){//skip title and comment strings;
-				strt_fl=1; if(debug) std::cout<<"\nhere!";
+				strt_fl=1; std::cout<<"\nhere!";
 			}
 			else if(strt_fl==1 && strstr((char*)ibuff,"END")==NULL){
-				if(debug) std::cout<<'\n'<<"line:"<<ibuff;
+				std::cout<<'\n'<<"line:"<<ibuff;
 				uc_arr_tkn=(unsigned char*)strtok((char*)ibuff,"+= ");
 				//std::cout<<'\n'<<"\nf_token="<<uc_arr_tkn<<" ("<<atoi(uc_arr_tkn)<<")";
 				unsigned int node_idx=atoi((char*)uc_arr_tkn);
-				if(debug) std::cout<<"\nnode_idx="<<node_idx;
+				std::cout<<"\nnode_idx="<<node_idx;
 				uc_arr_tkn=(unsigned char*)strtok(NULL,"+= ");
 				double d_scl=1.;
 				if(uc_arr_tkn[strlen((char*)uc_arr_tkn)-1]=='m') d_scl=0.001;//milli volts;
@@ -102,14 +89,14 @@ int main(int argc, char** argv){
 		std::cout<<"\nifh.is_open()";
 	
 	}
-	for(unsigned int i=0;i<num_nodes;i++) if(debug) std::cout<<"\nnode["<<i+1<<"]="<<node_voltages[i];
+	for(unsigned int i=0;i<num_nodes;i++) std::cout<<"\nnode["<<i+1<<"]="<<node_voltages[i];
 	
 	
 	
 	
 	//cout node voltages for Mathematica; skip first M nodes, because it is input (voltage) sources nodes;
-	std::cout<<"\nHspcOut=";
-	std::cout<<"{"<<node_voltages[M]; for(unsigned int i=M+1;i<num_nodes;i++) std::cout<<','<<node_voltages[i]; std::cout<<"};\n";
+	std::cout<<'\n';
+	std::cout<<"{"<<node_voltages[M]; for(unsigned int i=M+1;i<num_nodes;i++) std::cout<<','<<node_voltages[i]; std::cout<<"}\n";
 
 	return 0;
 }
